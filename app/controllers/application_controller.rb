e1e60e28a2da054627cc1a93864a8fe742a6276d
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-04-15 13:21:50 -0400

    Remove require mongo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
-require 'mongo'
-include Mongo
-
-mongo_uri = ENV['MONGOLAB_URI']
-db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
-client = MongoClient.from_uri(mongo_uri)
-db = client.db(db_name)
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
