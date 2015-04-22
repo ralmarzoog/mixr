db51ce8577b9c619f79fd1997cd26657223d4da7
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-04-22 13:20:01 -0400

    Fix voting model; display modification ratings

--- app/models/modification.rb
+++ app/models/modification.rb
@@ -7,21 +7,26 @@ class Modification
 
   validates :suggestion, presence: true, length: { minimum: 2 }
 
-  @vote_sum = 0
-
-  def vote_sum
-    @vote_sum
-  end
 
   def add_vote(vote_value) 
-    @vote_sum = @vote_sum || 0
+    self[:vote_sum] = self[:vote_sum] || 0
     vote = self.votes.new(value: vote_value)
     if vote.save!
-      @vote_sum += vote_value
+      self[:vote_sum] += vote_value
+      save!
     end
   end
 
   def rating
-    (@vote_sum and self.votes.count > 0) ? self.vote_sum / self.votes.count : 0
+    (self[:vote_sum] and self.votes.count > 0) ? self[:vote_sum] / self.votes.count.to_f : 0
   end
+
+  def vote_sum
+    self[:vote_sum]
+  end
+
+  private
+    def vote_sum=(val)
+      write_attribute :vote_sum, val
+    end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,5 +5,6 @@ class Vote
   # belongs_to :user # want it to have user id or something evenutally
   belongs_to :modification
 
-  validates :value, presence: true
+  validates :value, presence: true, numericality: { greater_than_or_equal_to: 0,
+                                                    less_than_or_equal_to: 5 }
 end
