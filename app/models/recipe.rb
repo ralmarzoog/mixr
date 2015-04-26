65ad3fd7e5901c9eadfedd8e5146ea49d5de1abb
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-04-26 19:21:07 -0400

    Try adding content type validation to image files

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,6 +11,7 @@ class Recipe
   accepts_nested_attributes_for :recipe_ingredients, :steps
 
   has_mongoid_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   private
   
