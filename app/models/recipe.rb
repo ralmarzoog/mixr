c9318c8f14feab9abb0913a220e626ee7cac7974
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-04-27 14:23:10 -0400

    Limit image size

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -12,6 +12,8 @@ class Recipe
 
   has_mongoid_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_size :image, :less_than => (0.05).megabytes, 
+                          :unless => Proc.new {|m| m[:image].nil?}
 
   private
   
