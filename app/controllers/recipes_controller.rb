c33f88fe34c7b2f00164b89ca54eecfa5c1929df
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-04-29 13:30:21 -0400

    Use sendgrid

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,6 +64,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes/1/edit
   def edit
+    UserMailer.new_modification("reema.almarzoog@gmail.com").deliver
   end
 
   # POST /recipes
