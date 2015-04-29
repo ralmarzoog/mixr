524b6d2237f5e5e245b61dddd1f1a34cc8964239
Reema Al-Marzoog <reema.almarzoog@gmail.com>
2015-04-29 17:00:18 -0400

    Improve mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "mixr@mixr.com"
 
-  def new_modification(email)
-    mail(to: email, subject: "New Modification for your Recipe")
+  def new_modification(email, recipe)
+    @recipe = recipe
+    @link = "https://fathomless-eyrie-3707.herokuapp.com/recipes/" + recipe.id
+    subject = "A new modification for #{recipe.title} was added to Mixr!"
+    mail(to: email, subject: subject)
   end
 end
